680180cf8a2682a58d6fb8c6658ba4ced37dfdb1
deceser <aufhaummtobsc@gmail.com>
2023-05-30 15:21:16 +0300

    add modal

---
+++ src/components/block/googleAuth/GoogleModalWrap.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import styles from "./GoogleModalWrap.module.scss";
+
+const GoogleModalWrap = ({ children, ...props }) => {
+  const { isCloseModal, setVisible } = props;
+
+  const visibleClass = [styles.modalui];
+
+  // if (setVisible) {
+  //   visibleClass.push(styles.active);
+  // } else if (setVisible === false) {
+  //   visibleClass.push(styles.closed);
+  // }
+
+  return (
+    <div className={[styles.modalui, styles.active].join(" ")}>
+      <div className={styles.modalContent}>{children}</div>
+    </div>
+  );
+};
+
+export default GoogleModalWrap;
--- src/layouts/default/index.jsx
+++ src/layouts/default/index.jsx
@@ -3,14 +3,19 @@ import React from "react";
 import Header from "src/components/block/header";
 import Footer from "src/components/block/footer";
 
+import GoogleModalWrap from "src/components/block/googleAuth/GoogleModalWrap";
 import NavigationBar from "src/components/block/navigation";
 
 import styles from "./index.module.scss";
 
 const DefaultLayout = ({ children, ...props }) => {
   const {} = props;
+  const [visibleSingUp, setVisibleSingUp] = React.useState(false);
+  const [visibleSingIn, setVisibleSingIn] = React.useState(false);
+
   return (
     <>
+      <GoogleModalWrap>sdfdsfsdfsdf</GoogleModalWrap>
       <Header />
       <div className={styles.navigation}>
         <NavigationBar />
