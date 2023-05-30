f907b5a455be2a2c06b07a3d70699c6573a3fa7f
deceser <aufhaummtobsc@gmail.com>
2023-05-30 21:00:29 +0300

    google auth not working

--- src/components/block/header/index.jsx
+++ src/components/block/header/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 import { useDispatch, useSelector } from "react-redux";
 import { getUser } from "src/redux/slices/googleAuthSlice";
@@ -10,15 +11,31 @@ import Logo from "src/components/ui/logo";
 import SmallButton from "src/components/ui/buttons/smallbutton";
 
 import styles from "./index.module.scss";
-import axios from "axios";
 
 const Header = ({ ...props }) => {
   const dispatch = useDispatch();
+  const [targetPath, setTargetPath] = React.useState("");
+  const navigate = useNavigate();
 
   const google = async () => {
     // const res = await axios.get("https://api.qualyfid.ai/google");
     // console.log(res);
-    window.location.href = "https://api.qualyfid.ai/google";
+    // window.location.href = "https://api.qualyfid.ai/google";
+
+    setTargetPath("http://localhost:5173/");
+
+    // Выполняете запрос к API Google для авторизации
+    try {
+      window.location.href = "https://api.qualyfid.ai/google";
+
+      // Здесь должна быть логика обработки успешной авторизации
+
+      // Перенаправление на целевой путь после успешной авторизации
+      navigate(targetPath);
+    } catch (error) {
+      // Обработка ошибки авторизации
+      console.error(error);
+    }
   };
 
   return (
